ce4618f5d5db0c3f38c668519f790ced3d687695
Arnav Komaragiri <komaraac@mail.uc.edu>
2023-03-23 21:32:46 -0400

    started untested ant implementation, still need to write up actual ant decision logic

---
+++ ant.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+from network import Lattice_Network
+
+class Ant:
+    def __init__(self, vec: np.ndarray, pos: tuple, alpha: float, beta: float, delta: float):
+        # initialize ant with the document vector
+        self.vec = vec
+        self.pos = pos
+
+        # initialize hyperparams
+        self.alpha = alpha
+        self.beta = beta
+        self.delta = delta
+
+    def decide_next_position(self, network: Lattice_Network):
+        # TODO: Implement this
+        pass
+
+    # do a roulette wheel decision with weighted probabilities
+    def roulette_wheel(self, probs: np.ndarray, num_samples: int = 1) -> float:
+        return np.random.choice(np.arange(len(probs)), num_samples, p=probs)
+
+    # apply ant pheremone weighting
+    def pheremone_weighting(self, sigma: float) -> float:
+        # TODO: are we doing the ant path smoothing factor here? See equation (9) in Fernandes et al.
+        return 1 + ((self.delta / (1 + (sigma * self.delta))) ** self.beta)
+
+    # compute the pheremone of the edge between two nodes (equation (10) in Fernandes et al.)
+    def find_edge_pheremone(self, centroid_pheremone: np.ndarray, node_pheremone: np.ndarray) -> float:
+        unweight_pheremone = np.linalg.norm(centroid_pheremone - node_pheremone)
+        return self.pheremone_weighting(unweight_pheremone)
+
+    # get pheremone update reinforce (R in equation (12) in Fernandes et al.)
+    def get_update_reinforce(self, centroid_pheremone: np.ndarray, node_pheremone: np.ndarray) -> float:
+        return self.alpha * (1 - (np.linalg.norm(centroid_pheremone - node_pheremone) / len(node_pheremone)))
+
+    # update the pheremone vector for the specified node (equation (11) in Fernandes et al.)
+    def get_new_pheremone_vec(self, centroid_pheremone: np.ndarray, node_pheremone: np.ndarray) -> np.ndarray:
+        reinforce = self.get_update_reinforce(centroid_pheremone, node_pheremone)
+        return node_pheremone + reinforce * (self.vec - node_pheremone)
--- network.py
+++ network.py
@@ -1,7 +1,14 @@
 import numpy as np
 
+from typing import List, Tuple
+
 class Lattice_Network():
-    def __init__(self, network_shape: tuple, embedding_dimension: int):
+    def __init__(self, network_shape: Tuple, embedding_dimension: int, evap_factor: float, 
+                 centroid_radius: int = 1):
+        # initialize centroid radius and evaporation factor
+        self.centroid_radius = centroid_radius
+        self.evap_factor = evap_factor
+
         if type(network_shape) != tuple:
             raise ValueError('network_shape should be a numpy shape (i.e. a tuple)')
     
@@ -10,8 +17,9 @@ class Lattice_Network():
 
         # initialize pheromone vectors
         unnormalized_pheromones = np.random.rand(network_shape[0], network_shape[1], embedding_dimension) 
-        # normalize each node's pheromone vector so it sums to 1.0 
-        self.pheromones = unnormalized_pheromones / np.sum(unnormalized_pheromones, axis=2, keepdims=2)
+        # normalize each node's pheromone vector to an L2-norm of 1
+        self.pheromones = unnormalized_pheromones / np.linalg.norm(unnormalized_pheromones, axis=2, keepdims=True)
+        self.init_pheremones = np.copy(self.pheremones)
 
         # initialize 8-neighbor neighborhood adjacency list
         self.neighbors = np.ndarray(network_shape, dtype=list)
@@ -49,5 +57,22 @@ class Lattice_Network():
 
                 if col != network_shape[1] - 1:
                     east = (row, col + 1)
-                    self.neighbors[row][col].append(east)\
-                    
+                    self.neighbors[row][col].append(east)
+    
+    def get_pheremone_vec(self, row: int, col: int) -> np.ndarray:
+        return self.pheremones[row][col]
+
+    def get_centroid_pheremone_vec(self, row: int, col: int) -> np.ndarray:
+        # get max row and column of the pheremone map
+        r, c, _ = self.pheromones.shape
+        min_row, max_row = max(0, row - self.centroid_radius), min(r - 1, row + self.centroid_radius)
+        min_col, max_col = max(0, col - self.centroid_radius), min(c - 1, col + self.centroid_radius)
+        # extract all pheremones in the target region
+        region_pheremones = self.pheremones[min_row:max_row][min_col:max_col]
+        return np.mean(region_pheremones, axis=[0, 1])
+    
+    def get_neighbors(self, row: int, col: int) -> List[Tuple]:
+        return self.neighbors[row][col]
+
+    def evaporate_pheremones(self):
+        self.pheremones -= self.evap_factor * self.init_pheremones
