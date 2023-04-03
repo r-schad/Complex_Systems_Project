50484fd7e65a4e4b06f50c2498ff84a70ebb0b4d
Arnav Komaragiri <komaraac@mail.uc.edu>
2023-04-03 12:53:29 -0400

    added penalization terms to change in movement

--- ant.py
+++ ant.py
@@ -1,24 +1,38 @@
+import math
 import argparse
 
 import numpy as np
 import matplotlib.pyplot as plt
 
+from matplotlib import cm
 from network import LatticeNetwork
 from tqdm import tqdm
+from typing import Tuple
 
 rng = np.random.default_rng(seed=0)
 
 class Ant:
-    def __init__(self, vec: np.ndarray, pos: tuple, alpha: float, beta: float, delta: float, eps: float = 0.01):
+    def __init__(self, vec: np.ndarray, pos: tuple, alpha: float, beta: float, delta: float, 
+                 eps: float = 0.01, move_base: float = 2.0):
         # initialize ant with the document vector
         self.vec = vec
         self.pos = pos
+        self.prev_pos = None
         self.eps = eps
 
         # initialize hyperparams
         self.alpha = alpha
         self.beta = beta
         self.delta = delta
+        self.move_base = move_base
+
+    def get_move_diff(self, candidate: Tuple):
+        if self.prev_pos is None:
+            return 1.0
+        curr_move = tuple(candidate[i] - self.pos[i] for i in range(len(self.pos)))
+        prev_move = tuple(self.pos[i] - self.prev_pos[i] for i in range(len(self.pos)))
+        angle_diff = math.atan2(*curr_move) - math.atan2(*prev_move)
+        return self.move_base ** (-abs(angle_diff))
 
     def decide_next_position(self, network: LatticeNetwork, q: float = 0.2) -> bool:
         # compute neighbors and corresponding pheromone levels
@@ -27,6 +41,9 @@ class Ant:
         pheromone_vecs = [network.get_pheromone_vec(r, c) for r, c in neighbors]
         edge_pheromones = [self.find_edge_pheromone(centroid_vecs[i], pheromone_vecs[i]) for i in range(len(neighbors))]
         pheromones = [self.pheromone_weighting(sigma) for sigma in edge_pheromones]
+        # compute the pheremone scalars for the change in directions
+        move_diffs = [self.get_move_diff(n) for n in neighbors]
+        pheromones = [move_diffs[i] * p for i, p in enumerate(pheromones)]
 
         # compute current node pheromones
         current_pheromone = self.find_edge_pheromone(network.get_centroid_pheromone_vec(*self.pos), 
@@ -41,15 +58,12 @@ class Ant:
                 stopped = True
         else:
             # TODO: Figure out if stochastic stop is necessary
-            # rescale the pheremone list to a 
-            # probs = np.array(pheromones + [current_pheromone]) / (np.sum(pheromones) + current_pheromone)
             probs = np.array(pheromones) / np.sum(pheromones)
             i = int(self.roulette_wheel(probs))
-            # if i == len(neighbors):
-            #     stopped = True
         if not stopped:
             new_pos = neighbors[i]
             # TODO: Implement previous move tracking
+            self.prev_pos = self.pos
             self.pos = new_pos
         return stopped
 
@@ -89,7 +103,9 @@ def parse_args():
     args.add_argument("-e", "--embedding-dim", type=int, default=5)
     args.add_argument("-c", "--num-classes", type=int, default=5)
     args.add_argument("-v", "--evaporation-factor", type=float, default=0.99)
-    args.add_argument("-s", "--num-steps", type=int, default=10000)
+    args.add_argument("-s", "--num-steps", type=int, default=600)
+    args.add_argument("-m", "--warmup-steps", type=int, default=200)
+    args.add_argument("-q", "--greedy-prob", type=float, default=0.2)
     args.add_argument("-z", "--zeros", action='store_true')
     return args.parse_args()
 
@@ -100,6 +116,7 @@ if __name__ == "__main__":
     existing_locs = set()
     ants, status = [], []
     vec_set = np.eye(args.num_classes, args.embedding_dim)
+    ant_set = [[] for _ in range(args.num_classes)]
     for i in range(args.num_ants):
         j = rng.choice(args.num_classes)
         ant_vec = vec_set[j] + rng.normal(scale=0.2, size=args.embedding_dim)
@@ -109,6 +126,7 @@ if __name__ == "__main__":
                 existing_locs.add(loc)
                 break
         ants += [(i, Ant(ant_vec, loc, 1, args.beta, args.delta))]
+        ant_set[j] += [i]
         status += [False]
 
     for i, s in enumerate(status):
@@ -119,19 +137,52 @@ if __name__ == "__main__":
         rng.shuffle(ants)
         for j, ant in ants:
             network.deposit_pheromone(ant.vec, *ant.pos)
-            if not status[j] and (i / args.num_steps) > 0.02:
-                status[j] = ant.decide_next_position(network, 0.5)
-            elif (i / args.num_steps) <= 0.02:
-                ant.decide_next_position(network, 0.5)
+            if not status[j] and i > args.warmup_steps:
+                status[j] = ant.decide_next_position(network, args.greedy_prob)
+            elif i <= args.warmup_steps:
+                ant.decide_next_position(network, args.greedy_prob)
         network.evaporate_pheromones()
         if all(status):
             break
 
-    for i, s in enumerate(status):
-        print(f"Status {i}: {status[i]}, Pos: {ants[i][1].pos}")
+    for i, ant in ants:
+        final_pheromone_vec = network.get_pheromone_vec(*ant.pos)
+        pheromone = ant.find_edge_pheromone(final_pheromone_vec, ant.vec)
+        print(f"Status {i}: {status[i]}, Pos: {ant.pos}, Final Pheromone: {pheromone}")
 
     diffs = [ants[0][1].pheromone_weighting(np.linalg.norm(network.pheromones - vec, axis=-1)) for vec in vec_set]
+    min_diffs = [np.min(diff) for diff in diffs]
+    for j, ant_idxs in enumerate(ant_set):
+        class_ants = [ant for idx, ant in ants if idx in ant_idxs]
+        for ant in class_ants:
+            r, c = ant.pos
+            diffs[j][r, c] = min_diffs[j] / 2
+
     fig, ax = plt.subplots(1, args.num_classes) 
     for i, diff in enumerate(diffs):
         ax[i].imshow(diff)
+    plt.show()
+
+    new_ant_class = int(input("Provide New Ant Class: "))
+    new_pos = tuple(rng.choice(np.arange(args.width), 2))
+    new_ant = Ant(vec_set[new_ant_class], new_pos, 1, args.beta, args.delta)
+
+    pos_seq = []
+    while True:
+        pos_seq += [new_ant.pos]
+        status = new_ant.decide_next_position(network, q=args.greedy_prob)
+        pheromone = new_ant.find_edge_pheromone(network.get_pheromone_vec(*new_ant.pos), new_ant.vec)
+        if pheromone > (0.8 * np.max(diffs[new_ant_class])):
+            break
+
+    final_match = new_ant.find_edge_pheromone(network.get_pheromone_vec(*new_ant.pos), new_ant.vec)
+    print(f"Path Length: {len(pos_seq)}")
+    print(f"Final Match: {final_match}")
+    path_diff = ants[0][1].pheromone_weighting(np.linalg.norm(network.pheromones - vec_set[new_ant_class], axis=-1))
+    diff_min = np.min(path_diff)
+    base = diff_min / 2
+    for i, pos in enumerate(pos_seq):
+        r, c = pos
+        path_diff[r, c] = base + ((diff_min - base) * (i / len(pos_seq)))
+    plt.imshow(path_diff)
     plt.show()
